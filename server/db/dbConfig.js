f9c8797fe0bd08db7051b0cc4928a79f2820d476
Matthew Goldwater <mattgwater@gmail.com>
2016-12-12 15:41:22 -0800

    Set conditional to determine whether to use production or deployment database

--- server/db/dbConfig.js
+++ server/db/dbConfig.js
@@ -4,14 +4,19 @@ var Sequelize = require('sequelize');
 var bcrypt = require('bcryptjs');
 var Promise = require('bluebird');
 console.log('database url is: ', process.env.JAWSDB_URL);
-var sequelize = new Sequelize(process.env.JAWSDB_URL, {
-  dialect: 'mysql',
-  pool: {
-    max: 5,
-    min: 0,
-    idle: 300000
-  }
-});
+
+if (process.env.JAWSDB_URL) {
+  var sequelize = new Sequelize(process.env.JAWSDB_URL, {
+    dialect: 'mysql',
+    pool: {
+      max: 5,
+      min: 0,
+      idle: 300000
+    }
+  });
+} else {
+  var sequelize = new Sequelize('persistentPoplar', 'root', '');
+}
 
 //Company table
 var Company = sequelize.define('company', {
